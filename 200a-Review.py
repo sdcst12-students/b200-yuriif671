d7a1c14d16123d3c21d2d503acef1f5ada3ac039
Chris Yang <cyang@deltalearns.ca>
2022-10-11 10:01:15 -0700

    initial assignment

---
+++ 200a-Review.py
@@ -0,0 +1,65 @@
+#!python3
+
+
+def getIntegers(myList):
+    # myList : expected list or tuple
+    # iterate through myList and add all the integers to the new list
+    integers = []
+
+    return integers
+
+def getFactor(myList,factor):
+    # myList : expected list or tuple
+    # factor : integer
+    # iterate through the list and add the number to the list if
+    # it is a factor of the number
+    factors = []
+
+    return factors
+
+def getNegatives(myList):
+    # myList : expected list or tuple
+    # iterate through myList and add all the negative numbers to the new list
+    negatives = []
+
+    return negatives
+
+def getIntersection(list1,list2):
+    # list 1: expected list or tuple
+    # list 2: expected list or tuple
+    # return a list of numbers that is in both lists
+    # the intersection of the 2 number sets
+    common = []
+
+    return common
+
+def getUnion(list1,list2):
+    # list 1: expected list or tuple
+    # list 2: expected list or tuple
+    # return a list of numbers that is in either of the lists
+    # the union of the 2 number sets
+    union = []
+
+    return union   
+
+def getMerge(list1,list2):
+    # list 1: expected list or tuple
+    # list 2: expected list or tuple
+    # add the elements of list2 into list1
+    # if the list2 element is in list1, add it at the position where it occurs in list1
+    # if the list2 element is not in list1, add it to the end
+    merge = list.copy()
+
+    return merge
+
+
+def main():
+    easy1 = [5,10,15,2,4,6,8]
+    easy2 = [-2,-4,-6,2,4,6,0.1]
+    numbers1 = [3,5,8,12,11,19,10,7,2,15,25,34,16,32,50,60,100,-3,0.25]
+    numbers2 = [3,7,11,15,19,23,27,31,35,39,44,50]
+
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# 200-review-lists-and-tuples
+# Review of Tuples and lists
+
+Requirements
+
+You need to remember the following:
+* difference between a tuple and a list
+* how to access the member elements of a tuple and list
+* how to use a for loop with a tuple and list
+* how to make a copy of a tuple as a list (or vice versa)
+
+Working with lists:
+Some important commands to know:
+listname.append()
+listname.index()
+listName.insert()
+listName.count()
+listName.pop()
+listName.reverse()
+listName.sort()
+
+Assignment:
+Open up the file called 200a-Review.py and create the code to make each function work.  You will need to create your own function calls in the main block to test out your functions.  You may use the existing lists to test your functions, or create some of your own.
